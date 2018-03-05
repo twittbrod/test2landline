cc6dfed11f7c7842d6235238077cdd640a6f85ea
Tim Wittbrod <twittbrod@gmail.com>
2018-03-05 07:05:42 -0500

    testing

--- text2landline.js
+++ text2landline.js
@@ -13,30 +13,40 @@ app.use(bodyParser());
 var listenPort = process.env.PORT || 3010;
 
 app.post('/', (req, res) => {
+    console.log("\n\n**************************************************************************");
+    console.log("req:");
+    console.log(req);
+    console.log("\n\n**************************************************************************");
+    console.log("req.body:");
+    console.log(req.body);
+    console.log("\n\n**************************************************************************");
+
+    /*
     var splittext = req.body.url.split('callerid=');
-var callerid = splittext[1];
-var messagebody = req.body.TranscriptionText;
+    var callerid = splittext[1];
+    var messagebody = req.body.TranscriptionText;
 
-console.log("Request received:");
-console.log("callerid: " + callerid);
-console.log("messagebody: " + messagebody);
+    console.log("Request received:");
+    console.log("callerid: " + callerid);
+    console.log("messagebody: " + messagebody);
 
-// send an SMS
-client.messages.create({
-        body: messagebody,
-        to: callerid,  // Text this number
-        from: process.env.TWILIO_NUMBER // From a valid Twilio number
-    },
-    function (error, message) {
-        if (error) {
-            // print error to console
-            console.log(error);
-        } else {
-            // print message sid to console
-            console.log("message sent: " + message.sid);
+    // send an SMS
+    client.messages.create({
+            body: messagebody,
+            to: callerid,  // Text this number
+            from: process.env.TWILIO_NUMBER // From a valid Twilio number
+        },
+        function (error, message) {
+            if (error) {
+                // print error to console
+                console.log(error);
+            } else {
+                // print message sid to console
+                console.log("message sent: " + message.sid);
+            }
         }
-    }
-)
+    )
+    */
 });
 
 
